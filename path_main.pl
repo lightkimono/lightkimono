d12a9e38f2987a65f176a04cd7f6ba4339711ba3
bulb 🐣 <kimonokrimson@gmail.com>
2024-07-26 22:47:57 +0700

    path_main.pl

--- path_main.pl
+++ path_main.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/local/bin/perl/inc
 # perlpath.pl
 # This script gets run only from setup.sh in order to replace the 
 # #!/usr/local/bin/perl line at the start of scripts with the real path to perl
